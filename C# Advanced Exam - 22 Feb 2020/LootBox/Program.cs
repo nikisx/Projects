493ec7e62b6785d9c2f4121165f0a80aa50a524d
nikisx <64860672+nikisx@users.noreply.github.com>
2020-05-05 21:04:58 +0300

    Add files via upload

---
+++ C# Advanced Exam - 22 Feb 2020/LootBox/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstBoxInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] secondBoxInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> firstBox = new Queue<int>(firstBoxInput);
+
+            Stack<int> secondBox = new Stack<int>(secondBoxInput);
+
+            var claimedItems = new List<int>();
+            while (true)
+            {
+                if (firstBox.Count <= 0 || secondBox.Count<=0)
+                {
+                    break;
+                }
+
+                int currFirst = firstBox.Peek();
+                int currSecond = secondBox.Peek();
+
+                if ((currFirst+currSecond) % 2 == 0)                
+                {
+                    claimedItems.Add(currFirst + currSecond);
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(currSecond);
+                    secondBox.Pop();
+                    
+                }
+            }
+
+            if (firstBox.Count<=0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondBox.Count<=0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+           int loot= claimedItems.Sum();
+
+            if (loot >=100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {loot}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {loot}");
+            }
+        }
+    }
+}
---
+++ C# Advanced Exam - 22 Feb 2020/Re-Volt/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countOFCommands = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            bool Won = false;
+
+            //filling
+            for (int row = 0; row < n; row++)
+            {
+                char[] currentRow = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                    if (currentRow[col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+            for (int i = 0; i < countOFCommands; i++)
+            {
+
+                string commands = Console.ReadLine();
+
+                if (commands == "up")
+                {
+                    if (isValid(playerRow - 1, playerCol, n))
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerRow--;
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            playerRow--;
+                        }
+                        else if (matrix[playerRow, playerCol] == 'T')
+                        {
+                            playerRow++;
+                        }
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerRow = n - 1;
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+
+                    }
+                }
+                else if (commands == "down")
+                {
+                    if (isValid(playerRow + 1, playerCol, n))
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerRow++;
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            playerRow++;
+                        }
+                        else if (matrix[playerRow, playerCol] == 'T')
+                        {
+                            playerRow--;
+                        }
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerRow = 0;
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                }
+                else if (commands == "right")
+                {
+                    if (isValid(playerRow, playerCol + 1, n))
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerCol++;
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            playerCol++;
+                        }
+                        else if (matrix[playerRow, playerCol] == 'T')
+                        {
+                            playerCol--;
+                        }
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerCol = 0;
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                }
+                else if (commands == "left")
+                {
+                    if (isValid(playerRow, playerCol - 1, n))
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerCol--;
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            if (isValid(playerRow, playerCol - 1, n))
+                            {
+                                playerCol--;
+                            }
+                            else
+                            {
+                                playerCol = n - 1;
+                            }
+                        }
+                        else if (matrix[playerRow, playerCol] == 'T')
+                        {
+                            playerCol++;
+                        }
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerCol = n - 1;
+                        if (matrix[playerRow, playerCol] == 'F')
+                        {
+                            matrix[playerRow, playerCol] = 'f';
+                            Won = true;
+                            break;
+                        }
+                        matrix[playerRow, playerCol] = 'f';
+                    }
+                }
+            }
+
+            if (Won == false)
+            {
+                Console.WriteLine("Player lost!");
+            }
+            else
+            {
+                Console.WriteLine("Player won!");
+            }
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static bool isValid(int row, int col, int n)
+        {
+            return row >= 0 && row < n && col >= 0 && col < n ? true : false;
+        }
+    }
+}
